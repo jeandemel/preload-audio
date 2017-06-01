bdf1202700fc0b27e310c33082050205fbeead12
Jean D <demeljean@hotmail.fr>
2017-06-01 15:34:54 +0200

    initial commit

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+// let audio = new Audio('https://upload.wikimedia.org/wikipedia/commons/b/bb/Test_ogg_mp3_48kbps.wav');
+
+// audio.preload = "auto";
+
+let ctx = new AudioContext();
+let source = ctx.createBufferSource();
+
+
+file.onload = function(e) {
+    e.target.result;
+};
+
+fetch(
+    'https://upload.wikimedia.org/wikipedia/commons/b/bb/Test_ogg_mp3_48kbps.wav'
+).then((response) => {
+    response.arrayBuffer().then((buff) => {
+        ctx.decodeAudioData(buff).then((audiobuff) => {
+            source.connect(ctx.destination);
+            source.buffer = audiobuff;
+            source.start();
+        });
+    });
+});
